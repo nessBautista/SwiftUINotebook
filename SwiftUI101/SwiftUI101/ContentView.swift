a667359a5ec1688f8260598d29034bced8b90ce7
NessBautista <nestor.jbautista@gmail.com>
2020-10-13 09:26:08 -0500

    complete gitignore

---
+++ SwiftUI101/SwiftUI101/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI101
+//
+//  Created by Nestor Javier Hernandez on 10/13/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI101/SwiftUI101/SwiftUI101App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI101App.swift
+//  SwiftUI101
+//
+//  Created by Nestor Javier Hernandez on 10/13/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI101App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
